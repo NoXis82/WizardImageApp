4c6ca1ada658760502ebd08dfba637be52fd188e
Slava Tumanov <vn.tumanov@me.com>
2020-09-23 23:35:32 +0300

    add layout listview, add method initView(), add method onClick for imageview

--- app/src/main/java/com/example/wizardimageapp/MainActivity.java
+++ app/src/main/java/com/example/wizardimageapp/MainActivity.java
@@ -2,13 +2,81 @@ package com.example.wizardimageapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int GALLERY_REQUEST = 1;
+    private ImageView imageView;
+    private TextView imageViewText;
+    private Button rotateBtn;
+    private Button invertBtn;
+    private Button mirrorBtn;
+    private ListView listItemAction;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        clickOpenImageFileBtn();
     }
+
+    private void initView() {
+        imageView = findViewById(R.id.imageView);
+        imageViewText = findViewById(R.id.imageViewText);
+        rotateBtn = findViewById(R.id.rotateBtn);
+        invertBtn = findViewById(R.id.invertBtn);
+        mirrorBtn = findViewById(R.id.mirrorBtn);
+        listItemAction = findViewById(R.id.list_item_action);
+    }
+
+    private void clickOpenImageFileBtn() {
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                    photoPickerIntent.setType("image/*");
+                    startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        Bitmap bitmap = null;
+        switch (requestCode) {
+            case GALLERY_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    imageView.setImageBitmap(bitmap);
+                }
+        }
+    }
+
 }
